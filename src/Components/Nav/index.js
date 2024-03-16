2863430905b3781448ebbf304effeaceca99bf21
boody <bodymagdy97@gmail.com>
2024-03-17 01:03:02 +0200

    s

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -26,7 +26,7 @@ export default function index() {
     
             {/* {links} */}
             <ul className= {`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[2]
-             left-0 w-full md:w-auto md:pl-0 pl-9 bg-white transition-all duration-500 ease-in ${isopen ? 'top-12' : 'top-[-490px]'}`}>
+             left-0 w-full md:w-auto md:pl-0 pl-9 bg-white transition-all duration-500 ease-in ${isopen ? 'top-18' : 'top-[-490px]'}`}>
             
              <li className='font-semibold my-7 md:my-0 md:ml-8'>
                 <Link to="">Home</Link>
