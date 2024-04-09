dfcf4eeae3aaba9db5a9c43dacc9f1ecd4a67e24
boody <bodymagdy97@gmail.com>
2024-04-10 01:13:13 +0200

    cat

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
-import Nav from '../src/Components/Nav/index.js'
+import Nav from "../src/Components/Nav/index.js";
 import { Route, Routes, BrowserRouter, Outlet } from "react-router-dom";
-import HomePage from './pages/HomePage'
-import About from './pages/About/index.js'
-import Services from './pages/Services/index.js'
-import Contact from './pages/Contact/index.js'
-import Doctors from './pages/Doctors/index.js'
-import APPOINTMENT from './Components/APPOINTMENT/index.js'
-import Footer from './Components/Footer'
-import Notfound from './Components/Notfound/Notfound.js';
-import Login from './pages/Login/Login.js';
-import Register from './pages/Register/Register.js';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+import HomePage from "./pages/HomePage";
+import About from "./pages/About/index.js";
+import Services from "./pages/Services/index.js";
+import Contact from "./pages/Contact/index.js";
+import Doctors from "./pages/Doctors/index.js";
+import APPOINTMENT from "./Components/APPOINTMENT/index.js";
+import Footer from "./Components/Footer";
+import Notfound from "./Components/Notfound/Notfound.js";
+import Login from "./pages/Login/Login.js";
+import Register from "./pages/Register/Register.js";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { RecoilRoot } from "recoil";
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Nav/>
-        <Routes>
-          <Route path="/">
-            <Route index element={<HomePage/>} />
-            <Route path="about" element={<About/>} />
-            <Route path="services" element={<Services/>} />
-            <Route path="contact" element={<Contact/>} />
-            <Route path="APPOINTMENT" element={<APPOINTMENT/>} />
-            <Route path="doctors" element={<Doctors/>} />
-            <Route path="register" element={<Register/>} />
-            <Route path="login" element={<Login/>} />
-            <Route path="*" element={<Notfound/>} />
-          </Route>
-        </Routes>
-        <Footer/>
-        <Outlet />
-      </BrowserRouter>
-        <ToastContainer />
+      <RecoilRoot>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/">
+              <Route index element={<HomePage />} />
+              <Route path="about" element={<About />} />
+              <Route path="services" element={<Services />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="APPOINTMENT" element={<APPOINTMENT />} />
+              <Route path="doctors" element={<Doctors />} />
+              <Route path="register" element={<Register />} />
+              <Route path="login" element={<Login />} />
+              <Route path="*" element={<Notfound />} />
+            </Route>
+          </Routes>
+          <Footer />
+          <Outlet />
+        </BrowserRouter>
+      </RecoilRoot>
+      <ToastContainer />
     </div>
   );
 }
