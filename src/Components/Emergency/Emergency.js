3fd67f5d3cc0e11aab0abbd12298ddb6d927cd7e
boody <bodymagdy97@gmail.com>
2024-04-12 15:27:50 +0200

    redy

--- src/Components/Emergency/Emergency.js
+++ src/Components/Emergency/Emergency.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Emergency.scss'
 import { Link } from 'react-router-dom';
+import $AuthData from '../../store/index'
+import { useRecoilState } from "recoil";
 export default function Emergency() {
+  const [authRecoil] = useRecoilState($AuthData)
+
   return (
     <div>
     <div className='emergency'>
@@ -9,7 +13,12 @@ export default function Emergency() {
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
             mollit anim id est laborum.</p>
+            {authRecoil.isAuth ? 
         <Link className='btn' to="/APPOINTMENT">تحديد موعد</Link>
+            
+            :(<Fragment>
+                <Link className='btn' to="/Register">go to register</Link>
+            </Fragment>)}
     </div>
     </div>
   )
